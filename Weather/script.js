951f3eb18e0ac32dd9d9cc633b5fde3872a859f0
Atharv Patharkar <69659480+Atharv180@users.noreply.github.com>
2024-02-11 16:57:00 +0530

    Add files via upload

---
+++ Weather/script.js
@@ -0,0 +1,53 @@
+/*const TML = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';*/
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "16ba79cb5bmsh5c492cdd0f960bcp10c608jsnb0ccd5f5ba48",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com"
+  }
+};
+/*
+(async ()=>{
+try {
+	const respoTML = await fetch(url, options);
+	const resTML = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+})()
+
+*/
+const getWeather = (city) => 
+{
+	cityName.innerHTML = city
+  fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city , options)
+    .then(response => response.json())
+
+
+    .then((response) => {
+    console.log(response);
+      //cloud_pct.innerHTML = response.cloud_pct;
+    temp.innerHTML = response.temp
+	  temp2.innerHTML = response.temp
+    feels_like.innerHTML = response.feels_like
+    humidity.innerHTML = response.humidity
+	  humidity2.innerHTML = response.humidity
+    min_temp.innerHTML = response.min_temp
+      max_temp.innerHTML = response.max_temp
+      wind_speed.innerHTML = response.wind_speed
+	  wind_speed2.innerHTML = response.wind_speed
+      wind_degrees.innerHTML = response.wind_degrees
+      sunrise.innerHTML = response.sunrise
+      sunset.innerHTML = response.sunset
+    })
+
+    .catch(err => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+	e.preventDefault()
+    getWeather(city.value);
+});
+
+getWeather("Delhi")
